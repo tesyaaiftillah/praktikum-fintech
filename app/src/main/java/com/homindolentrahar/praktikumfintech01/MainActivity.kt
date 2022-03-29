1bf6c90520b32af60cdf9da1624cf6f2c77f6c9c
Rahardiyan E. Widiatmoko <rahardiyanekowidiatmoko@gmail.com>
2022-03-29 19:33:24 +0700

    Praktikum 4 - Fragment

---
+++ app/src/main/java/com/homindolentrahar/praktikumfintech01/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.homindolentrahar.praktikumfintech01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btnhome = findViewById<Button>(R.id.btn_home)
+        val btnprofile = findViewById<Button>(R.id.btn_profile)
+        val btnsettings = findViewById<Button>(R.id.btn_settings)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame, HomeFragment())
+            .commitNow()
+
+        btnhome.setOnClickListener {
+            LoadFragment(HomeFragment())
+        }
+        btnprofile.setOnClickListener {
+            LoadFragment(ProfileFragment())
+        }
+        btnsettings.setOnClickListener {
+            LoadFragment(SettingsFragment())
+        }
+    }
+
+    private fun LoadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.frame, fragment).commitNow()
+
+    }
+}
